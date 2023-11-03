099febdddf2ea2861c3ced45a3397a8602034f5e
rushipalakudtewar <rushipalakudtewar8329@gmail.com>
2023-11-03 23:41:45 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-4xl font-bold">
+        <p>
+          Hello world
+        </p>
+    </div>
+  );
+}
+
+export default App;
